e42cbedd005734acfad812b25ec295c6d9bca52e
Brandon Goose <beegee54321@gmail.com>
2026-02-15 00:36:06 -0800

    Added to router/health.js next auth.js

---
+++ routes/health.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({ status: "OK", message: "API is healthy"});
+});
+
+module.exports = router;
