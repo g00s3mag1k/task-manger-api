bffaa4e92651ae87055e88b43058f7a3b445035c
Brandon Goose <beegee54321@gmail.com>
2026-02-16 02:38:47 -0800

    Added cors localhost port to server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const connectDB = require("./config/db");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true, }));
 app.use(express.json());
+
 
 // connect DB
 connectDB();
