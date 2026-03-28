23b05f78622a220471d182ced9aea0c355b636d7
Brandon Goose <beegee54321@gmail.com>
2026-03-28 13:05:59 -0700

    Added to cors frontend URL

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const connectDB = require("./config/db");
 const app = express();
 
 // middleware
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://task-manager-frontend-r18l.onrender.com",
+];
 
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        }, credentials: true,
+    }));
+app.use(express.json());
 
 // connect DB
 connectDB();
